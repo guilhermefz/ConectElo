9672f0a745411fd8082d3abd3b5f13029b36712a
Guilherme Fiuza <guilherme_f_campos@estudante.sesisenai.org.br>
2026-02-26 17:13:50 -0300

    feat: Adicionado interface base de repositório genérico

---
+++ src/ConectElo.Domain/Areas/Base/Interfaces/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace ConectElo.Domain.Areas.Base.Interfaces
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        IQueryable<TEntity> Consultar();
+        Task<TEntity?> SelecionarPorId(Guid id);
+        Task Inserir(TEntity entity);
+        void Excluir(TEntity entity);
+        void Atualizar(TEntity entity);
+        Task<int> CommitAsync();
+    }
+}
---
+++ src/ConectElo.Infra/Areas/Repositories/Base/RepositoryGeneric.cs
@@ -0,0 +1,48 @@
+﻿using ConectElo.Domain.Areas.Base.Interfaces;
+using ConectElo.Infra.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConectElo.Infra.Areas.Repositories.Base
+{
+    public abstract class RepositoryGeneric<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        protected readonly AppDbContext _context;
+        protected readonly DbSet<TEntity> _dbSet;
+
+        protected RepositoryGeneric(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+
+        public virtual void Atualizar(TEntity entity)
+        {
+           _dbSet.Update(entity);
+        }
+
+        public async Task<int> CommitAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public virtual IQueryable<TEntity> Consultar()
+        {
+            return _dbSet.AsNoTracking();
+        }
+
+        public virtual void Excluir(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public async Task Inserir(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public virtual async Task<TEntity?> SelecionarPorId(Guid id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+    }
+}
