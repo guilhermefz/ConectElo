253e77e1ebf838560eea85cf4dd6ea59f055a721
Guilherme Fiuza <guilherme_f_campos@estudante.sesisenai.org.br>
2026-03-11 17:11:38 -0300

    feat(social): implementar registro customizado de usuários com UserManager

---
+++ src/ConectElo.Application/Areas/Social/DTOs/RegistrarUsuarioDto.cs
@@ -0,0 +1,16 @@
+﻿using ConectElo.Domain.Areas.Social.Enuns;
+
+namespace ConectElo.Application.Areas.Social.DTOs
+{
+    public class RegistrarUsuarioDto
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string password { get; set; }
+        public string? CPF { get; set; }
+        public string? FotoPerfillUrl { get; set; }
+        public DateOnly DataNascimento { get; set; }
+        public GeneroEnum Genero { get; set; }
+        public string? Bio { get; set; }
+    }
+}
--- src/ConectElo.Application/Areas/Social/Mappers/UsuarioProfile.cs
+++ src/ConectElo.Application/Areas/Social/Mappers/UsuarioProfile.cs
@@ -1,8 +1,17 @@
 ﻿using AutoMapper;
+using ConectElo.Application.Areas.Social.DTOs;
+using ConectElo.Domain.Areas.Social.Entities;
 
 namespace ConectElo.Application.Areas.Social.Mappers
 {
     public class UsuarioProfile : Profile
     {
+        public UsuarioProfile()
+        {
+            CreateMap<Usuario, RegistrarUsuarioDto>()
+                .ForMember(dest => dest.password, opt => opt.MapFrom(src => src.PasswordHash))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.UserName))
+                .ReverseMap();
+        }
     }
 }
