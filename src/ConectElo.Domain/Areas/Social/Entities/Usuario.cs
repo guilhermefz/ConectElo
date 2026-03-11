1c247d1c63f94e5b4745952eaab069012e39df75
Guilherme Fiuza <guilherme_f_campos@estudante.sesisenai.org.br>
2026-03-11 17:10:11 -0300

    feat: herdar entidade Usuario de IdentityUser<Guid> e limpeza de campos redundantes

--- src/ConectElo.Domain/Areas/Social/Entities/Usuario.cs
+++ src/ConectElo.Domain/Areas/Social/Entities/Usuario.cs
@@ -1,20 +1,23 @@
 ﻿using ConectElo.Domain.Areas.Base;
 using ConectElo.Domain.Areas.Social.Enuns;
+using Microsoft.AspNetCore.Identity;
 
 namespace ConectElo.Domain.Areas.Social.Entities
 {
-    public class Usuario : EntityBase
+    public class Usuario : IdentityUser<Guid>
     {
+        public Usuario()
+        {
+            Id = Guid.NewGuid();
+        }
+
         public string Nome { get; set; }
         public string? CPF { get; set; }
-        public string Email { get; set; }
-        public string Telefone { get; set; }
         public string? FotoPerdilUrl { get; set; }
         public DateOnly DataNascimento { get; set; }
-        public DateTime DataCriacaoConta { get; set; }
+        public DateTime DataCriacaoConta { get; set; } = DateTime.UtcNow;
         public GeneroEnum Genero { get; set; }
-        public string SenhaHash { get; set; }
-        public DateTime UltimaAtualizacao { get; set; }
+        public DateTime UltimaAtualizacao { get; set; } = DateTime.UtcNow;
         public bool UsuarioAtivo { get; set; }
         public string? Bio {  get; set; }
         public DateTime? DataDelecao { get; set; }
