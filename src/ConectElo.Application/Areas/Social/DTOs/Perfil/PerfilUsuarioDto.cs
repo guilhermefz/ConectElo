36819514f168085347552ba716c37115de67a2b4
Guilherme Fiuza <guilherme_f_campos@estudante.sesisenai.org.br>
2026-05-05 15:08:09 -0300

    feat: adicionado DTOs de perfil para transferência de dados entre camadas

---
+++ src/ConectElo.Application/Areas/Social/DTOs/Perfil/PerfilUsuarioDto.cs
@@ -0,0 +1,15 @@
+﻿using ConectElo.Domain.Areas.Social.Enuns;
+
+namespace ConectElo.Application.Areas.Social.DTOs.Perfil
+{
+    public class PerfilUsuarioDto
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string? Bio {  get; set; }
+        public string? FotoPerfilUrl { get; set; }
+        public DateOnly DataNascimento { get; set; }
+        public GeneroEnum Genero { get; set; }
+    }
+}
---
+++ src/ConectElo.Application/Areas/Social/DTOs/Perfil/UploadFotoDto.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ConectElo.Application.Areas.Social.DTOs.Perfil
+{
+    public class UploadFotoDto
+    {
+        public IFormFile Foto { get; set; }
+    }
+}
