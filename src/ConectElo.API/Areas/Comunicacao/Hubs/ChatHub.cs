875e7990ae64bd67d5204a1cebdd8e07ec919ff2
Guilherme Fiuza <guilherme_f_campos@estudante.sesisenai.org.br>
2026-05-08 11:26:52 -0300

    fix: ajuste do nome de usuario para mensagens de chat

--- src/ConectElo.API/Areas/Comunicacao/Hubs/ChatHub.cs
+++ src/ConectElo.API/Areas/Comunicacao/Hubs/ChatHub.cs
@@ -1,6 +1,7 @@
 ﻿using ConectElo.Application.Areas.Comunicacao.InterfacesService;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 
 namespace ConectElo.API.Areas.Comunicacao.Hubs
@@ -23,9 +24,17 @@ namespace ConectElo.API.Areas.Comunicacao.Hubs
         public async Task EnviarMensagem(string grupoId, string conteudo)
         {
             var usuarioId = Guid.Parse(Context.User!.FindFirstValue(ClaimTypes.NameIdentifier)!);
+            var nomeAutor = Context.User!.FindFirstValue(JwtRegisteredClaimNames.Name) ?? "";
             var mensagem = await _mensageService.EnviarMensagemAsync(Guid.Parse(grupoId), usuarioId, conteudo);
 
-            await Clients.Group(grupoId).SendAsync("ReceberMensagem", mensagem);
+            await Clients.Group(grupoId).SendAsync("ReceberMensagem", new
+            {
+                mensagem.Id,
+                mensagem.Conteudo,
+                NomeAutor = nomeAutor,
+                UsuarioId = usuarioId,
+                mensagem.HorarioEnvio
+            });
         }
     }
 }
