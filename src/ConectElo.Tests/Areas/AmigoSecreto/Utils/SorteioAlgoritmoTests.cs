e75c04d69f4ab92bfe7fdd88eafd43e3be211188
Guilherme Fiuza <guilherme_f_campos@estudante.sesisenai.org.br>
2026-07-07 16:23:11 -0300

    test: add xUnit test project and test for Sortear with invalid participant count

---
+++ src/ConectElo.Tests/Areas/AmigoSecreto/Utils/SorteioAlgoritmoTests.cs
@@ -0,0 +1,19 @@
+﻿using ConectElo.Application.Areas.AmigoSecreto.Utils;
+using ConectElo.Domain.Exceptions;
+
+namespace ConectElo.Tests.Areas.AmigoSecreto.Utils
+{
+    public class SorteioAlgoritmoTests
+    {
+        [Fact]
+        public void Sortear_ComMenosDeDoisParticipantes_BusinessException()
+        {
+            var participantes = new List<Guid> { Guid.NewGuid() };
+
+            Action act = () => SorteioAlgoritmo.Sortear(participantes);
+
+            var exception = Assert.Throws<BusinessException>(act);
+            Assert.Equal("É necessário pelo menos 2 participantes para realizar o sorteio.", exception.Message);
+        }
+    }
+}
