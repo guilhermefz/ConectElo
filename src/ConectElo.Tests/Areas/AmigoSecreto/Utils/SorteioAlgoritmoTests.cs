bda68331f8da17fb81079eeabbc32ea541f4ce60
Guilherme Fiuza <guilherme_f_campos@estudante.sesisenai.org.br>
2026-07-07 17:06:45 -0300

    test: add tests for duplicate participants and returns same pair count

--- src/ConectElo.Tests/Areas/AmigoSecreto/Utils/SorteioAlgoritmoTests.cs
+++ src/ConectElo.Tests/Areas/AmigoSecreto/Utils/SorteioAlgoritmoTests.cs
@@ -15,5 +15,34 @@ namespace ConectElo.Tests.Areas.AmigoSecreto.Utils
             var exception = Assert.Throws<BusinessException>(act);
             Assert.Equal("É necessário pelo menos 2 participantes para realizar o sorteio.", exception.Message);
         }
+
+        [Fact]
+        public void Sortear_ComParticipantesDuplicados_BusinessException()
+        {
+            var participantesId = new List<Guid> { Guid.NewGuid() };
+            participantesId.Add(participantesId[0]);
+
+            Action act = () => SorteioAlgoritmo.Sortear(participantesId);
+
+            var exception = Assert.Throws<BusinessException>(act);
+            Assert.Equal("Existem participantes duplicados na lista.", exception.Message);
+        }
+
+        [Fact]
+        public void Sortear_ComParticipantesValidos_DeveRetornarPares()
+        {
+            var pessoa1 = Guid.NewGuid();
+            var pessoa2 = Guid.NewGuid();
+            var participantes = new List<Guid> { pessoa1, pessoa2 };
+
+            var pares = SorteioAlgoritmo.Sortear(participantes);
+
+            Assert.Equal(participantes.Count, pares.Count);
+            Assert.All(pares, par =>
+            {
+                Assert.Contains(par.Presenteador, participantes);
+                Assert.Contains(par.Recebedor, participantes);
+            });
+        }
     }
 }
